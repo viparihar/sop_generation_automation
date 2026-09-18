8b9d7c21d984e08d635fe10e14e97cc30d60e3e8
Parihar <viraj-singh.parihar@capgemini.com>
2026-09-18 15:33:38 +0530

    shifted to gemini llm

--- backend/llm_service.py
+++ backend/llm_service.py
@@ -4,15 +4,22 @@ from dotenv import load_dotenv
 
 from langchain_mistralai import ChatMistralAI
 
+from langchain_google_genai import ChatGoogleGenerativeAI
+
 from .prompts import sop_prompt
 
 from .models import SOPModel
 
 load_dotenv()
 
-llm = ChatMistralAI(
-    model = "mistral-small-2603",
-    api_key = os.getenv("MISTRAL_API_KEY")
+# llm = ChatMistralAI(
+#     model = "mistral-small-2603",
+#     api_key = os.getenv("MISTRAL_API_KEY")
+# )
+
+llm = ChatGoogleGenerativeAI(
+    model = "gemini-3.1-flash-lite",
+    api_key = os.getenv("GOOGLE_API_KEY")
 )
 
 structured_llm = llm.with_structured_output(
